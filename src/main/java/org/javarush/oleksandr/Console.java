516bf6cbac7c1b3746a3c518ff81b250ad9a2115
Oleksandr <oleksandr.jr@gmail.com>
2023-03-10 18:24:08 +0100

    v1.1

--- src/main/java/org/javarush/oleksandr/Console.java
+++ src/main/java/org/javarush/oleksandr/Console.java
@@ -23,10 +23,7 @@ public class Console {
     }
 
     public static class color {
-        // Reset
         public static final String RESET = "\033[0m";  // Text Reset
-
-        // Regular Colors
         public static final String BLACK = "\033[0;30m";   // BLACK
         public static final String RED = "\033[0;31m";     // RED
         public static final String GREEN = "\033[0;32m";   // GREEN
